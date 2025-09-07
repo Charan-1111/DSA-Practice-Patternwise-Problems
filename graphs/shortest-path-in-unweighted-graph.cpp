1fadcf6e8e977b7e5e45d6f235da5cc1a57f4c0f
Charan <ravivijayacharan0@gmail.com>
2025-09-07 20:46:18 +0530

    printing all binary strings

---
+++ graphs/shortest-path-in-unweighted-graph.cpp
@@ -0,0 +1,10 @@
+// Q . Given a undirected, unweighted graph of V nodes and E edges, a source node S , and a destination node D, 
+// we need to find the shortest path from the source node to the destitnation node.
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	
+}
---
+++ recursion/generate-all-binary-strings.cpp
@@ -0,0 +1,52 @@
+/*
+	Q . Given an integer n. Generte and print all the binary strings of length n that does not contains consecutive 1's
+	A binary string is that which contains only 1's and 0's
+
+	Example 1:
+
+	Input:
+	n = 3
+	Output:
+	000 , 001 , 010 , 100 , 101
+	Explanation:
+	None of the above strings contain consecutive 1s. "110" is not an answer as it has '1's occuring consecutively. 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int i, int n, string s, vector<string> &res) {
+	if(i >= n) {
+		res.push_back(s);
+		return;
+	}
+
+	// include 0 in the current string;
+	solve(i+1, n, s+"0", res);
+
+	// include 1 in the current string, but before including the 1 we need to check if the previous character should not be 1
+	if(s.back() != '1')
+		solve(i+1, n, s+"1", res);
+}
+
+vector<string> genearateStrings(int n) {
+	if(n == 0) return {};
+
+	string s = "";
+	vector<string> res;
+
+
+	sovle(0, n, s, res);
+
+	return res;
+}
+
+int main() {
+	int n;
+	cout << "Enter the length of the binary strings : ";
+	cin >> n;
+
+	vector<string> binaryStrings = genearateStrings(n);
+
+}
