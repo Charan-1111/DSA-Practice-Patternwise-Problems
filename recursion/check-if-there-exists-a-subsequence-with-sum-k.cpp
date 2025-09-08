46b520748185660c0549d894fac478d005f9a7ab
Charan <ravivijayacharan0@gmail.com>
2025-09-08 22:29:02 +0530

    check if there exists a subsequence with sum k (both recursive and dp solution)

---
+++ recursion/check-if-there-exists-a-subsequence-with-sum-k.cpp
@@ -0,0 +1,68 @@
+/*
+	Q. Given an array arr of size n and sum k, check whether there exists a subsequence such that the sum of all elements in the subsequence equals the given target sum k
+
+	Example 1:
+
+	Input:  arr = [10,1,2,7,6,1,5], k = 8.
+	Output:  Yes
+	Explanation:  Subsequences like [2, 6], [1, 7] sum upto 8
+
+	Example 2 :
+
+	Input:  arr = [2,3,5,7,9], k = 100. 
+	Output:  No
+	Explanation:  No subsequence can sum upto 100
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool solve(int i, int n, vector<int> &arr, int k) {
+	if(k == 0) return true;
+
+	if(i >= n) {
+		return k == 0;
+	}
+
+
+	bool no_take = false, take = false;
+
+	// don't consider the current element in to the subset;
+	no_take = solve(i+1, n, arr, k);
+
+
+	// consider the current element into the subset
+
+	if(arr[i] <= k) 
+		take = solve(i+1, n, arr, k-arr[i]);
+
+
+	return take || no_take;
+}
+
+
+bool checkIfSubsequecnceExists(vector<int> &arr, int n, int k) {
+	return solve(0, n, arr, k);
+}
+
+int main() {
+	int n;
+	cout << "Enter the size of the array : ";
+	cin >> n;
+
+	cout << "Enter the elements of the array : ";
+	vector<int> arr(n);
+	for(int i=0; i<n; i++)
+		cin >> arr[i];
+
+	int k;
+	cout << "Enter the sum : ";
+	cin >> k;
+
+	return checkIfSubsequecnceExists(arr, n, k);
+}
+
+
+// Time Complexity - O(2^n)
+// Space complexity - O(1)
