aac1ec6c2bb0eee69a113870f5f737781e5fb700
Leela Guru Charan Avvaru <78796571+Charan-1111@users.noreply.github.com>
2025-10-11 20:25:15 +0530

    Create sort-LL.cpp

---
+++ linked-list/sort-LL.cpp
@@ -0,0 +1,101 @@
+/*
+    Q. Given a singly linked list, sort the list in ascending order.
+        
+    Example 1:
+       Input:  1 -2 3 1
+       Output: -2 1 1 3
+       
+    Example 2:
+        Input : 1 
+        Output : 1
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+  int data;
+  Node* next;
+  Node* prev;
+};
+
+Node* getNewNode(int data) {
+    Node* newNode = new Node(); 
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    
+    return newNode;
+}
+
+Node* insertIntoDLL(Node* head, int data) {
+    Node* newNode = getNewNode(data);
+    
+    if(!head) return newNode;
+    
+    Node* curr = head;
+    
+    while(curr->next) {
+        curr = curr->next;
+    }
+    
+    curr->next = newNode;
+    newNode->prev = curr;
+    
+    return head;
+}
+
+
+// ================> Main Logic <================
+Node* insert(Node* head, int data) {
+	Node* temp = (Node*) malloc(sizeof(Node));
+	temp->data = data;
+	temp->next = head;
+
+	return temp;
+}
+
+Node* sortLL(Node *head) {
+	vector<int> v;
+	Node* curr = head;
+	while(curr) {
+		v.push_back(curr->data);
+		curr = curr->next;
+	}
+
+	sort(v.begin(), v.end());
+	head = NULL;
+
+	for(int i=v.size()-1; i >= 0; i--) {
+		head = insert(head, v[i]);
+	}
+
+	return head;
+}
+// ================> Main Logic <================
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the doubly linked list : ";
+    cin >> n;
+    
+    Node* head = NULL;
+    
+    cout << "Enter the elements of the linked list in order : ";
+    for(int i=0; i<n; i++) {
+        int data;
+        cin >> data;
+        
+        head = insertIntoDLL(head, data);
+    }
+    
+    head = sortLL(head);
+    
+    return 0;
+}
+
+// Time complexity - O(n)
+// Space complexity - O(n * Sizeof(Node)) 
+
+// Try for better approach than this
